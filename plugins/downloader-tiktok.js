ed57d37d2f599414685f075683c7ac3978ece53e
MaxxyBotz <107167852+Maxxy-Md@users.noreply.github.com>
2022-10-02 13:27:37 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -18,7 +18,7 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
 ${description}
 ◈ ━┉┈┄┈┈ ►
 
-script : https://youtube.com/channel/UCjoPsysjCn2Qa0dRalUb2mg
+script : https://youtube.com/channel/UC6GPl9xMWL61NAXQb3HBrRw
 `.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
